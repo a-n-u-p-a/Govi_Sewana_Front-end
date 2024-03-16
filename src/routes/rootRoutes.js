f908389367f3906a6a54271780d9d1d5e3649a22
Nethmi11 <127391055+Nethmi11@users.noreply.github.com>
2024-03-16 14:08:41 +0530

    Revert "update "

--- src/routes/rootRoutes.js
+++ src/routes/rootRoutes.js
@@ -1,15 +1,10 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Welcome from "../pages/welcome/welcome";
-
-
-
 
 const RootRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Welcome/>}/>
 
             </Routes>
         </Router>
